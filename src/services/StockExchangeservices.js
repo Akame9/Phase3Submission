67dc80f769bfbfd9babd02e68e4458fd6e23d006
Aathira Pillai <aathira.pillai@cumminscollege.in>
2021-07-28 02:01:04 +0530

    JWT plus Cloud

--- src/services/StockExchangeservices.js
+++ src/services/StockExchangeservices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const SE_API_BASE_URL = "http://localhost:8080";
-//const SE_API_BASE_URL = "https://aathiraspringbootphase3.herokuapp.com";
+//const SE_API_BASE_URL = "http://localhost:8080";
+const SE_API_BASE_URL = "https://aathiraspringbootphase3.herokuapp.com";
 
 class SEservices {
 
